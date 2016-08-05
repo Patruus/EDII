9588ab0b611f7189ee18499896e4b0aee3f1b75a
icaaro.leite@gmail.com <icaaro.leite@gmail.com>
2016-08-05 01:53:04 -0300

    Interface basica do programa

---
+++ ordenacao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+char CMD = 'X';
+
+
+pritnf("		Bem vindo ao programa de ordenação \n");
+printf("		Selecione o algoritimo de ordenacao conforme a legenda abaixo \n");
+printf("		1- Selection Sort | 2- Insertion Sort | 3 - Shellsort | 4 - Quicksort \n");
+printf("		5 - HeapSort | Q - SAIR \n");
+scanf("%c", &CMD);
+
+}
